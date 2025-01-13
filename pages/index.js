3b30241b6c8ca8080be8310eeddaa046e8059be6
Ehtesham Ul Haq <ihtishamb7@gmail.com>
2025-01-13 19:33:22 +0500

    title

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Demo from "@/components/Demo";
 import Features from "@/components/Features";
 import Hero from "@/components/Hero";
+import Head from "next/head";
 
 
 export default function LandingPage() {
@@ -8,11 +9,17 @@ export default function LandingPage() {
  
   
   return (
+      <>
+      <Head>
+        <title>TextEase . Find Your Ease</title>
+        <link rel="icon" type="image/png" href="fevicon.webp" />
+      </Head>
     <div>
       <Hero />
       <Features />
       <Demo />
     </div>
+      </>
   );
 }
 
